246fd88381119c83bc0e988a34bcfb3fb64eff3a
Enver Onur Çoğalan <ericmoren@Enver-MacBook-Air.local>
2025-04-02 21:22:34 +0300

    :star: update: application improved

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Quizz.app' }} />
         <Stack.Screen name="Quiz" component={QuizzScreen} options={{ title: 'Quiz' }} />
         <Stack.Screen name="Result" component={ResultScreen} options={{ title: 'Success' }} />
